4b642ad217796a4916619755ce1b4bd33a7622d0
Puneesh-here <web.puneesh@gmail.com>
2021-08-03 22:03:32 +0530

    music player work    css and js

---
+++ music player/music.js
@@ -0,0 +1,20 @@
+const musicContanier = document.querySelector('.music-contaner')
+const playBtn = document.querySelector('#play')
+const prevBtn = document.querySelector('#prev')
+const nextBtn = document.querySelector('#next')
+const audio = document.querySelector('#audio')
+const progress = document.querySelector('.progress')
+const progressContaner = document.querySelector('.progresscontaner')
+const title = document.querySelector('#title')
+const cover = document.querySelector('#cover')
+
+//song titles
+const songs = ['hope', 'fun']
+
+//keep track of songs
+let songIndex = 1
+
+//initally load song info DOM
+loadsong(songs[songIndex])
+
+//update song details
